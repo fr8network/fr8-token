0ea5ff22b37c299571c1fc2271acbcc122f038a7
Yevgeniy Spektor <yevgeniy.spektor@gmail.com>
2018-06-12 12:18:44 -0400

    Updated shipment contract

--- contracts/ShipmentContract.sol
+++ contracts/ShipmentContract.sol
@@ -1,66 +1,122 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.21;
 
-import './deps/Ownable.sol';
-import './deps/SafeMath.sol';
-import '../node_modules/zeppelin-solidity/contracts/token//ERC20/ERC20Basic.sol';
-import '../node_modules/zeppelin-solidity/contracts/token/ERC20/SafeERC20.sol';
+import "./deps/Ownable.sol";
+import "./deps/SafeMath.sol";
+import "./deps/ERC20Basic.sol";
 
 contract ShipmentContract is Ownable {
-  using SafeMath for uint;
+    using SafeMath for uint256;
 
-  mapping(uint256 => string) possibleStates;
-  address beneficiary;
-  uint256 numStates;
-  uint256 currentStateIndex;
-  string public currentState;
-  uint256 shipmentValue;
-  uint256 weightLbs; 
-  uint256 numPieces; 
-  string poNumber; 
-  uint256 shipmentId; 
-  uint256 totalCost;
+    mapping(uint16 => string) public possibleStates;
+    uint16 public numStates;
+    uint16 public currentStateIndex;
+    string public currentState;
+    uint256 public shipmentValue;
+    uint256 public weightLbs; 
+    uint256 public numPieces; 
+    string public poNumber; 
+    string public bolNumber;
+    uint256 public shipmentId; 
+    uint256 public totalCost;
 
-  event ChangeStatus(string prevString, string currString);
+    mapping (uint16 => StateChangeMetadata) public stateChanges;
 
-  /**
-  Deploy args: all uint256
-  
-  3500,12000,6,18504502,49504020,400
-  */
+    address public beneficiary;
+    address public tokenAddress;
 
-  function settlePayments(uint256 amount) private {
-    ERC20 token = ERC20(0x8049F53D94eCf5D1e669f80977eecf075B461608);
-    token.transfer(beneficiary, amount*(10**18));
-  }
+    // 1.0% FR8 Fee
+    uint256 FR8_FEE_PERCENTAGE = 10;
+    uint256 FEE_BASIS_POINTS = 1000;
+    address FR8_FEE_ADDRESS = 0x11278a8b29109eafe3e41d73a9a94c2e3358caab;
 
-  function ShipmentContract(uint256 _shipmentValue, uint256 _weightLbs, uint256 _numPieces, string _poNumber, uint256 _shipmentId, uint256 _totalCost, address _beneficiary) {
-    numStates = 5;
-    possibleStates[0] = "SHIPMENT_BOOKED";
-    possibleStates[1] = "CARRIER_ASSIGNED";
-    possibleStates[2] = "EN_ROUTE_TO_DESTINATION";
-    possibleStates[3] = "ARRIVED_AT_DESTINATION";
-    possibleStates[4] = "SHIPMENT_DELIVERED";
+    event StateChanged(string prevString, string currString);
 
-    currentStateIndex = 1;
-    currentState = possibleStates[currentStateIndex];
-    shipmentValue = _shipmentValue;
-    weightLbs = _weightLbs; 
-    numPieces = _numPieces; 
-    poNumber = _poNumber; 
-    shipmentId = _shipmentId;  
-    totalCost = _totalCost;
-    beneficiary = _beneficiary;
-  }
-
-  function nextState() public returns (string shipmentState) {
-    require(currentStateIndex < (numStates - 1));
-    string memory prevState = possibleStates[currentStateIndex];
-    currentStateIndex = currentStateIndex.add(1);
-    currentState = possibleStates[currentStateIndex];
-    if (currentStateIndex == (numStates - 1)) {
-      settlePayments(totalCost);
+    struct StateChangeMetadata {
+        uint256 lon;
+        uint256 lat;
+        string comment;
     }
-    emit ChangeStatus(prevState, currentState);
-    return shipmentState;
-  }
+
+    function ShipmentContract(
+        uint256 _shipmentValue,
+        uint256 _weightLbs,
+        uint256 _numPieces,
+        string _poNumber,
+        string _bolNumber,
+        uint256 _shipmentId,
+        uint256 _totalCost,
+        address _beneficiary,
+        address _tokenAddress)
+        public {
+        numStates = 0;
+        currentStateIndex = 0;
+        currentState = "DEPLOYED";
+        shipmentValue = _shipmentValue;
+        weightLbs = _weightLbs;
+        numPieces = _numPieces;
+        poNumber = _poNumber;
+        bolNumber = _bolNumber;
+        shipmentId = _shipmentId;
+        totalCost = _totalCost;
+        beneficiary = _beneficiary;
+        tokenAddress = _tokenAddress;
+    }
+
+    /**
+     * @dev Ensures sure the correct of number of tokens have been deposited to contract
+     */
+    modifier tokensDeposited() {
+        ERC20Basic Token = ERC20Basic(tokenAddress);
+        uint256 balance = Token.balanceOf(address(this));
+        require (totalCost <= balance);
+        _;
+    }
+
+    /**
+     * @dev Appends _state string to possible states mapping
+     */
+    function addState(string _state) public onlyOwner {
+        possibleStates[numStates] = _state;
+        if (numStates == 0) {
+            currentState = _state;
+        }
+        numStates++;
+    }
+
+    /**
+     * @dev Internal function to settle shipment payment
+     */
+    function settlePayment() private {
+        ERC20Basic Token = ERC20Basic(tokenAddress);
+        uint256 balance = Token.balanceOf(address(this));
+        uint256 fr8FeeAmount = balance.mul(FR8_FEE_PERCENTAGE).div(FEE_BASIS_POINTS);
+        uint256 payout = balance.sub(fr8FeeAmount);
+        Token.transfer(beneficiary, payout);
+        Token.transfer(FR8_FEE_ADDRESS, fr8FeeAmount);
+    }
+
+    /**
+     * @dev Internal function to settle shipment payment
+     */
+    function nextState(uint256 _lon, uint256 _lat, string _comment) public onlyOwner tokensDeposited returns (string shipmentState) {
+        // Make sure shipment is not in last state
+        require(currentStateIndex < (numStates - 1));
+
+        // Save state change meta data
+        stateChanges[currentStateIndex] = StateChangeMetadata(_lon, _lat, _comment);
+
+        // Shift states
+        string memory prevState = possibleStates[currentStateIndex];
+        currentStateIndex++;
+        currentState = possibleStates[currentStateIndex];
+
+        // Settle payment if shipment in last state
+        if (currentStateIndex == (numStates - 1)) {
+            settlePayment();
+        }
+
+        // Emit StateChanged Event
+        emit StateChanged(prevState, currentState);
+        return shipmentState;
+    }
 }
